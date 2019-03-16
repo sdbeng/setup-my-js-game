c6b214c8fc38e074b8b5ab1ebc9885984bf0b181
Sergio Barrera <sdbeng@gmail.com>
2019-03-16 10:25:20 -0700

    Add pseudo code starter steps

---
+++ step1.js
@@ -0,0 +1,30 @@
+// GLOBAL VARIABLES (accessible by all functions)
+// ==================================================================================================
+
+// Create an array of Word Options (all lowercase to make it simple for now)
+var wordsList = ["tevez", "gago", "benedetto", "simon", "maradona", "messi"];
+
+// solution will be stored in here.
+var chosenWord = "";
+
+// This will break the solution into individual letters to be stored in array.
+var lettersInChosenWord = [];
+
+// This will be the number of blanks we show as a hint based on the solution
+var numBlanks = 0;
+
+// Holds a mix of blank and solved letters (ex: 'm, _ _, s, _').
+var blanksAndSuccesses = [];
+// Holds all of the wrong guesses
+var wrongGuesses = [];
+
+// Game counters
+var winCounter = 0;
+var lossCounter = 0;
+var numGuesses = 9;
+
+//startGame
+
+//foo
+
+//bar
